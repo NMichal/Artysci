ca69d52ea984f34f2cfa0461e10292cecbd99932
Mateusz Lindel <mateusz.lindel@smcebi.edu.pl>
2017-06-01 19:59:42 +0200

    dodanie do object class profileLogin

---
+++ Artysci/ObjectsClass/profileLogin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Artysci.ObjectsClass
+{
+    class ProfileLogin
+    {
+        public int id_profile { get; set; }
+        public string login_user { get; set; }
+    }
+}
