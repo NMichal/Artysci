ddcc2d9901abef2ef975a6268efe639d9d3bca87
Bogdan Drzewiecki <Bogdan.drzewiecki93@gmail.com>
2017-06-06 15:23:41 +0200

    Dodano wyswietlanie ogloszen

--- Artysci/UserControls/ucAnnouncements.cs
+++ Artysci/UserControls/ucAnnouncements.cs
@@ -7,12 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Artysci.ObjectsClass;
+using System.Diagnostics;
+using Artysci.Forms;
 
 namespace Artysci.UserControls
 {
     public partial class ucAnnouncements : UserControl
     {
         private static ucAnnouncements _instance;
+        private List<Announ> announList;
+        AnnounControl aControl;
+        int index;
 
         public static ucAnnouncements Instance
         {
@@ -38,7 +44,7 @@ namespace Artysci.UserControls
         private void ucAnnouncements_Load(object sender, EventArgs e)
         {
             List<ObjectsClass.Announ> announs_tab = Database.getAnnouns();
-
+            announList = Database.getAnnouns();
             if (announs_tab.Count == 0)
             {
                 ListViewItem lvi = new ListViewItem("Brak ogłoszeń w systemie.");
@@ -49,7 +55,7 @@ namespace Artysci.UserControls
                 listView1.Items.Add(lvi);
             }
 
-            for(int i=0; i<announs_tab.Count; i++)
+            for (int i = 0; i < announs_tab.Count; i++)
             {
                 ListViewItem lvi = new ListViewItem(announs_tab[i].login_user);
                 lvi.SubItems.Add(announs_tab[i].type_anoun);
@@ -58,6 +64,17 @@ namespace Artysci.UserControls
                 lvi.SubItems.Add(announs_tab[i].date);
                 listView1.Items.Add(lvi);
             }
+        }
+
+        private void listView1_DoubleClick(object sender, EventArgs e)
+        {
+            Debug.WriteLine("halo");
+            annPanel.Controls.Remove(aControl);
+            index = listView1.SelectedIndices[0];
+            aControl = new AnnounControl(announList[index].title, announList[index].type_anoun, announList[index].type_looking, announList[index].town, announList[index].date, announList[index].descr);
+            aControl.OpenControl();
+            annPanel.Visible = true;
+            annPanel.Controls.Add(aControl);
         }
     }
 }
