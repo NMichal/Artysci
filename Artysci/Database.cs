7f21f0adf7391e345f3051bd1587a933f37918db
Mateusz Lindel <mateusz.lindel@smcebi.edu.pl>
2017-06-06 22:39:31 +0200

    poprawienie metody addsondChoice

--- Artysci/Database.cs
+++ Artysci/Database.cs
@@ -144,6 +144,7 @@ namespace Artysci
                         command.Parameters.Add(new SqlParameter("date_end", sond.date_end));
                         command.ExecuteNonQuery();
                     }
+                    con.Close();
 
                     addSondChoice(answers, nextId);
                 }
@@ -151,7 +152,7 @@ namespace Artysci
                 {
                     Console.WriteLine("Count not insert.");
                 }
-                con.Close();
+                
             }
         }
         
@@ -191,6 +192,7 @@ namespace Artysci
             try
             {
                 int nextid = getNewSondChoiceId().id;
+                if (nextid == 0) nextid = 1;
 
                 foreach (sondChoice item in answers)
                 {
